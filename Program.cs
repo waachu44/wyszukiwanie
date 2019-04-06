43b7ecdb86695755ecf75980036e5cea0e745442
Waachu44 <Waachu44@gmail.com>
2019-04-06 17:21:53 +0200

    my first repository

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Projekt1
+{
+    class Program
+    {
+        //Wyszukiwanie liniowe przed instrumentacją
+        private static bool isPresent(int[] Vector, int Number)
+        {
+
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                if (Vector[i] == Number)
+                    return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Stopwatch stopwatch = new Stopwatch();
+            string path = @"d:\x.txt";
+            StreamWriter sw = new StreamWriter(path);
+            int[] tab = new int[2000];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = rand.Next();
+                Array.Sort(tab);
+                stopwatch.Start();
+                isPresent(tab, 202313);
+                stopwatch.Stop();
+                stopwatch.Reset();
+                sw.WriteLine((i + 1) + " " + tab[i] + " " + stopwatch.ElapsedMilliseconds);
+
+            }
+            sw.Close();
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
